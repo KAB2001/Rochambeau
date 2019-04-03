297d85b3d647fcf6e70fa8f91831303eb4e8fe94
Killian A Brait <kb11814@srcs.loc>
2019-04-03 13:36:41 -0700

    defined classes and atributes stubs

---
+++ Rochambeau.js
@@ -0,0 +1,19 @@
+class Rochambeau {
+    constructor() {
+        this.player = new Player();
+        this.computer = new Player();
+        this.scoreboard = new Scoreboard();
+    }
+}
+
+class Player {
+    constructor() {
+        this.choice = "";
+    }
+}
+
+class Scoreboard {
+    constructor() {
+
+    }
+}
