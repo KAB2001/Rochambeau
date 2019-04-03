5a7f86d18e300001e61ac7835bbb6d15024a54e1
Killian A Brait <kb11814@srcs.loc>
2019-04-03 13:40:12 -0700

    defined methods for Rochambeau class

--- Rochambeau.js
+++ Rochambeau.js
@@ -4,6 +4,27 @@ class Rochambeau {
         this.computer = new Player();
         this.scoreboard = new Scoreboard();
     }
+
+    storePlayerChoice(choice) {
+
+    }
+
+    storeComputerChoice() {
+
+    }
+
+    determineOutcome() {
+        return "";
+    }
+
+    updateScoreboard() {
+
+    }
+
+    displayScoreboard() {
+
+    }
+
 }
 
 class Player {
